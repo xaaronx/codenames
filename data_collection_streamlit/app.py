7a9883c69e9865eac2f1ca9371fc97116b967ca8
Shafi <adam.shafi@capgemini.com>
2022-04-27 11:37:03 +0100

    updates

--- data_collection_streamlit/app.py
+++ data_collection_streamlit/app.py
@@ -38,82 +38,84 @@ Once you've selected your words, a box will appear for you to enter the clue. Ma
 st.subheader('Your Words')
 
 ### Load words ###
+
 def select_words():
     words = pd.read_csv(Path(__file__).parents[0] / 'word_list.csv').sample(n=5).values
     words = [i[0] for i in words]
     return words
 
+b = st.button('generate words')
 
-col1,col2,col3,col4,col5 = st.columns(5)
-st.session_state.words = select_words()
-col1.write(st.session_state.words[0])
-col2.write(st.session_state.words[1])
-col3.write(st.session_state.words[2])
-col4.write(st.session_state.words[3])
-col5.write(st.session_state.words[4])
+if b:
+    col1,col2,col3,col4,col5 = st.columns(5)
+    st.session_state.words = select_words()
+    col1.write(words[0])
+    col2.write(words[1])
+    col3.write(words[2])
+    col4.write(words[3])
+    col5.write(words[4])
 
-### Skip Button ###
-st.write("")
-if st.button(label = 'Skip these words'):
-    st.experimental_rerun()
-st.write("")
+    ### Skip Button ###
+    st.write("")
+    if st.button(label = 'Skip these words'):
+        st.experimental_rerun()
+    st.write("")
 
-### Select words ###
-with st.form("my_form", clear_on_submit=False):
-    st.write("Select your words")
+    ### Select words ###
+    with st.form("my_form", clear_on_submit=False):
+        st.write("Select your words")
 
-    word1 = st.checkbox(key = 'word1', label = st.session_state.words[0])
-    #word2 = st.checkbox(label = words[1])
-    #word3 = st.checkbox(label = words[2])
-    #word4 = st.checkbox(label = words[3])
-    #word5 = st.checkbox(label = words[4])
+        word1 = st.checkbox(label = words[0])
+        #word2 = st.checkbox(label = words[1])
+        #word3 = st.checkbox(label = words[2])
+        #word4 = st.checkbox(label = words[3])
+        #word5 = st.checkbox(label = words[4])
 
-    clue = st.text_input(label='Enter your single word clue:', value="")
-    submitted = st.form_submit_button("Submit")
+        clue = st.text_input(label='Enter your single word clue:', value="")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.write(word1)
 
-    if submitted:
-        st.write(st.session_state.word1)
+    a = st.button('press to continue')
+    if a:
+        selections = [word1,word2,word3,word4,word5]
 
-a = st.button('press to continue')
-if a:
-    selections = [word1,word2,word3,word4,word5]
+        st.write(selections)
 
-    st.write(selections)
+        selected_words = ';'.join(
+            [k for k,v in zip(words, selections) if v ==True])
+        unselected_words = ';'.join(
+            [k for k,v in zip(words, selections) if v ==False])
+        st.write(selections)
+        st.write("selected", selected_words, "unselected", unselected_words, "clue",clue)
+        datetimenow = datetime.now().strftime("%Y%m%d%H%M%S")
+        results = {
+            'selected_words':selected_words,
+            'unselected_words': unselected_words,
+            'clue':clue}
+        results = json.dumps(results)
+        file_name = 'data_{}.json'.format(datetimenow)
+        file_from = Path(__file__).parents[0] / file_name
+        with open(file_from, 'w') as f:
+            json.dump(results, f)
 
-    selected_words = ';'.join(
-        [k for k,v in zip(words, selections) if v ==True])
-    unselected_words = ';'.join(
-        [k for k,v in zip(words, selections) if v ==False])
-    st.write(selections)
-    st.write("selected", selected_words, "unselected", unselected_words, "clue",clue)
-    datetimenow = datetime.now().strftime("%Y%m%d%H%M%S")
-    results = {
-        'selected_words':selected_words,
-        'unselected_words': unselected_words,
-        'clue':clue}
-    results = json.dumps(results)
-    file_name = 'data_{}.json'.format(datetimenow)
-    file_from = Path(__file__).parents[0] / file_name
-    with open(file_from, 'w') as f:
-        json.dump(results, f)
-
-    #try:
-    creds = Credentials(token=st.secrets['token'],
-                        refresh_token = st.secrets['refresh_token'],
-                        token_uri = st.secrets['token_uri'],
-                        client_id=st.secrets['client_id'],
-                        client_secret=st.secrets['client_secret'],
-                        scopes=st.secrets['scopes'],
-                        expiry=datetime.strptime(st.secrets['expiry'], "%Y-%m-%dT%H:%M:%S"))
+        #try:
+        creds = Credentials(token=st.secrets['token'],
+                            refresh_token = st.secrets['refresh_token'],
+                            token_uri = st.secrets['token_uri'],
+                            client_id=st.secrets['client_id'],
+                            client_secret=st.secrets['client_secret'],
+                            scopes=st.secrets['scopes'],
+                            expiry=datetime.strptime(st.secrets['expiry'], "%Y-%m-%dT%H:%M:%S"))
 
 
-    service = build('drive', 'v3', credentials=creds)
-    file_metadata = {'name': file_name,
-                     'parents': ['150dCgltvQa5gKum45F-fPlhKZ_tG0347']}
-    media = MediaFileUpload(file_from)
-    file = service.files().create(body=file_metadata,
-                                  media_body=media,
-                                  fields='id').execute()
+        service = build('drive', 'v3', credentials=creds)
+        file_metadata = {'name': file_name,
+                         'parents': ['150dCgltvQa5gKum45F-fPlhKZ_tG0347']}
+        media = MediaFileUpload(file_from)
+        file = service.files().create(body=file_metadata,
+                                      media_body=media,
+                                      fields='id').execute()
 
 
 
