b02224dda8f84f843526e41e0795885ac1b6d28a
Shafi <adam.shafi@capgemini.com>
2022-04-27 10:21:32 +0100

    updates

--- data_collection_streamlit/app.py
+++ data_collection_streamlit/app.py
@@ -22,8 +22,8 @@ from googleapiclient.http import MediaFileUpload
 
 st.set_page_config(layout="wide")
 
-session_state = st.SessionState.get(random_number=random.random())
-st.write(session_state.random_number)
+state = st.session_state.get(random_number=random.random())
+st.write(state.random_number)
 
 st.title('Codenames Dataset')
 st.subheader('Instructions')
